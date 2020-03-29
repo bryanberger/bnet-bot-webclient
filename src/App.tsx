2bce5ff505ab32dcbd9c33b7fbe5a58df4ef6607
Bryan Berger <bryan@bryanberger.com>
2020-03-29 12:01:00 -0400

    add an App stub

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo } from 'react'
+import useWebSocket, { ReadyState } from 'react-use-websocket'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { AuthenticateRequest, ConnectRequest } from './services/CAPI/actions'
+import useClientContext from './providers/ClientProvider/hook'
+import { Chat, UserList, MessageBar } from './components'
+import AppLayout from './components/AppLayout'
+import EnterApiKey from './components/EnterApiKey'
+import { changeApiKey } from './providers/ClientProvider/actions'
+
+const socketUrl = 'wss://connect-bot.classic.blizzard.com/v1/rpc/chat'
+
+const connectionStatus = {
+  [ReadyState.CONNECTING]: 'Connecting',
+  [ReadyState.OPEN]: 'Open',
+  [ReadyState.CLOSING]: 'Closing',
+  [ReadyState.CLOSED]: 'Closed',
 }
 
-export default App;
+const App = () => {
+  const { state } = useClientContext()
+
+  return (
+    <>
+      {!state.apiKey && <EnterApiKey />}
+      <AppLayout />
+    </>
+  )
+
+  // const STATIC_OPTIONS = useMemo(
+  //   () => ({
+  //     onClose: (e: any) => console.log('onClose', e),
+  //     onMessage: (e: any) => console.log('onMessage', e),
+  //     onError: (e: any) => console.log('onError', e),
+  //     onOpen: (e: any) => {
+  //       console.log('onOpen', e)
+  //       e.target.send(AuthenticateRequest)
+  //     },
+  //     shouldReconnect: () => false,
+  //     reconnectAttempts: 3,
+  //     reconnectInterval: 10000,
+  //   }),
+  //   [],
+  // )
+
+  // const [messageHistory, setMessageHistory] = useState<MessageEvent[]>([])
+  // const [sendMessage, lastMessage, readyState, getWebSocket] = useWebSocket(
+  //   socketUrl,
+  //   STATIC_OPTIONS,
+  // )
+
+  // // const handleClickSendMessage = useCallback(() => sendMessage('Hello'), [])
+  // // useEffect(() => {
+  // //   if (readyState === ReadyState.OPEN) {
+  // //     sendMessage(AuthenticateRequest)
+  // //   }
+  // // }, [readyState])
+
+  // useEffect(() => {
+  //   if (lastMessage !== null) {
+  //     //getWebSocket returns the WebSocket wrapped in a Proxy. This is to restrict actions like mutating a shared websocket, overwriting handlers, etc
+  //     // const currentWebsocketUrl = getWebSocket().url
+  //     console.log('lastMessage useEffect', getWebSocket().url, lastMessage)
+  //     // console.log('received a message from ', currentWebsocketUrl)
+  //     switch (lastMessage.data.command) {
+  //       case 'Botapiauth.AuthenticateResponse':
+  //         return sendMessage(ConnectRequest)
+  //     }
+
+  //     setMessageHistory(prev => prev.concat(lastMessage))
+  //   }
+  // }, [sendMessage, lastMessage])
+
+  // return (
+  //   <div>
+  //     {/* <button
+  //       onClick={handleClickSendMessage}
+  //       disabled={readyState !== ReadyState.OPEN}
+  //     >
+  //       Click Me to send 'Hello'
+  //     </button> */}
+  //     {/* <span>Status: {connectionStatus}</span> */}
+  //     <span>Status: {connectionStatus[readyState]}</span>
+  //     {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+  //     <ul>
+  //       {messageHistory.map((message: MessageEvent, id: number) => (
+  //         <span key={id}>{message.data}</span>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // )
+}
+
+export default App
