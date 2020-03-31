92f926bc05bd9271b8bf1d4b639900d158c4fb5e
Bryan Berger <bryan@bryanberger.com>
2020-03-30 21:46:57 -0400

    add a useEffect stub to App.tsx (TODO: make a custom hook and wrap it all)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
 import React, { useState, useEffect, useMemo } from 'react'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
 
-import { AuthenticateRequest, ConnectRequest } from './services/CAPI/actions'
 import useClientContext from './providers/ClientProvider/hook'
-import { Chat, UserList, MessageBar } from './components'
 import AppLayout from './components/AppLayout'
 import EnterApiKey from './components/EnterApiKey'
-import { changeApiKey } from './providers/ClientProvider/actions'
-
-const socketUrl = 'wss://connect-bot.classic.blizzard.com/v1/rpc/chat'
-
-const connectionStatus = {
-  [ReadyState.CONNECTING]: 'Connecting',
-  [ReadyState.OPEN]: 'Open',
-  [ReadyState.CLOSING]: 'Closing',
-  [ReadyState.CLOSED]: 'Closed',
-}
+import {
+  changeApiKey,
+  changeMessages,
+  changeUsers,
+  setClient,
+} from './providers/ClientProvider/actions'
+import CAPIClient from './services/CAPI'
 
 const App = () => {
-  const { state } = useClientContext()
+  const { state, dispatch } = useClientContext()
+  useEffect(() => {
+    if (state.apiKey !== null) {
+      const client = new CAPIClient(state.apiKey)
+
+      client.on('ready', () => {
+        console.log('ready')
+        setClient(dispatch, client)
+        // if (client.localUser) changeLocalUser(dispatch, client.localUser)
+      })
+
+      client.on('channelJoin', e => {
+        console.log('channelJoin', e)
+      })
+
+      client.on('message', e => {
+        console.log('message', e)
+        changeMessages(dispatch, client.messages)
+      })
+
+      client.on('userJoin', e => {
+        console.log('userJoin', e)
+        changeUsers(dispatch, client.users)
+      })
+
+      client.on('userLeave', e => {
+        console.log('userLeave', e)
+      })
+
+      // const users = client.users
+    }
+    // if (state.apiKey != null) {
+    //   const ws = new WebSocket(socketUrl, 'json')
+    //   ws.onopen = () => {
+    //     console.log('onopen')
+    //     ws.send(AuthenticateRequest(state.apiKey as string))
+    //   }
+    //   ws.onmessage = e => {
+    //     const response = JSON.parse(e.data)
+    //     console.log('onmessage', response)
+    //     if (response.command == 'Botapiauth.AuthenticateResponse') {
+    //       console.log('try and connect...')
+    //       ws.send(ConnectRequest)
+    //     }
+    //   }
+    //   ws.onclose = e => console.log('close', e)
+    //   ws.onerror = e => console.log('error', e)
+    // }
+  }, [state.apiKey])
 
   return (
     <>
@@ -42,6 +85,24 @@ const App = () => {
   //   }),
   //   [],
   // )
+
+  // const [sendMessage, lastMessage, readyState, getWebSocket] = useWebSocket(
+  //   socketUrl,
+  //   STATIC_OPTIONS,
+  // )
+
+  // useEffect(() => {
+  //   if (lastMessage !== null) {
+  //     // getWebSocket returns the WebSocket wrapped in a Proxy. This is to restrict actions like mutating a shared websocket, overwriting handlers, etc
+  //     console.log('lastMessage useEffect', getWebSocket().url, lastMessage)
+  //     switch (lastMessage.data.command) {
+  //       case 'Botapiauth.AuthenticateResponse':
+  //         return sendMessage(ConnectRequest)
+  //     }
+
+  //     // setMessageHistory(prev => prev.concat(lastMessage))
+  //   }
+  // }, [sendMessage, lastMessage])
 
   // const [messageHistory, setMessageHistory] = useState<MessageEvent[]>([])
   // const [sendMessage, lastMessage, readyState, getWebSocket] = useWebSocket(
